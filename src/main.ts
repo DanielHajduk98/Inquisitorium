54bc69ad101fb91d3b331902b112c0e03b095835
DanielHajduk98 <danhajduk@protonmail.com>
2021-06-23 14:08:56 +0200

    Initial

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as ROT from "rot-js/lib/index";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App).use(store).use(router);
+
+app.config.globalProperties.$ROT = ROT;
+
+app.mount("#app");
